cf53b0dacbbcd855ba6e343a31ea66503c1ea38a
Amarababu400 <amarababuk0@gmail.com>
2025-02-27 20:10:02 -0600

    Update user_spec.rb

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -6,18 +6,30 @@ RSpec.describe User do
   describe '#log_in' do
     it 'logs the user in' do
       # Fill in the test
+      # Arrange
+      user = User.new("test_user")
+      user.log_in
+      expect(user.logged_in).to be true
     end
   end
 
   describe '#log_out' do
     it 'logs the user out' do
       # Fill in the test
+  
+      user = User.new("test_user")
+      user.log_in  # First, log in the user
+      user.log_out
+      expect(user.logged_in).to be false
     end
   end
 
   describe '#username' do
     it 'returns the correct username' do
       # Fill in the test
+      user = User.new("test_user")
+
+      expect(user.username).to eq("test_user")
     end
   end
 end
